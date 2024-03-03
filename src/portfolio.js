2490c77545a826685c19757677de93f00f2507bb
Akash S <akashajay510@gmail.com>
2024-03-04 00:16:02 +0530

    personal web

---
+++ src/portfolio.js
@@ -0,0 +1,694 @@
+/* Change this file to get your personal Porfolio */
+
+// Website related settings
+const settings = {
+  isSplash: false, // Change this to true if you want to use the splash screen.
+  useCustomCursor: true, // Change this to false if you want the good'ol cursor
+  googleTrackingID: "UA-174238252-2",
+};
+
+//Home Page
+const greeting = {
+  title: "Hello.",
+  title2: "Akash",
+  logo_name: "Akashfolio",
+  nickname: "Akash",
+  full_name: "Akash S",
+  subTitle:
+    "Full Stack Developer, DSA Enthusiast, Cisco CA, Chair IEEE SKCET, COE STI.",
+    
+  resumeLink:
+    "https://drive.google.com/file/d/1yiFtyHXFHkLjBIfUKB98MCzX_qO-xEWL/view?usp=drivesdk",
+  mail: "mailto:akashsweb@gmail.com",
+};
+
+const socialMediaLinks = {
+  /* Your Social Media Link */
+  github: "",
+  linkedin: "http://www.linkedin.com/in/akashnb",
+  gmail: "mailto:akashsweb@gmail.com",
+  github: "https://github.com/akashnb0",
+  instagram: "http://instagram.com/nb_akash_",
+  contact: "http://instagram.com/nb_akash_",
+};
+
+const skills = {
+  data: [
+    {
+      title: "Full Stack Development",
+      fileName: "FullStackImg",
+      skills: [
+        "🔹  Building responsive website front end using ReactJS",
+        "🔹  Developing applications using Google App Script",
+        "🔹  Backend development using NodeJS & RestAPI with Springboot",
+      ],
+      softwareSkills: [
+       
+       
+        
+        // {
+        //   skillName: "Java",
+        //   fontAwesomeClassname: "simple-icons: Java",
+        //   style: {
+        //     color: "#f89820",
+        //   },
+        // },
+        
+        {
+          skillName: "C++",
+          fontAwesomeClassname: "simple-icons:cplusplus",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "MySQL",
+          fontAwesomeClassname: "simple-icons:mysql",
+          style: {
+            color: "#4479A1",
+          },
+        },
+        {
+          skillName: "Postman",
+          fontAwesomeClassname: "simple-icons:postman",
+          style: {
+            color: "#4479A1",
+          },
+        },
+        {
+          skillName: "C",
+          fontAwesomeClassname: "simple-icons:c",
+          style: {
+            color: "#E94E32",
+          },
+        },
+
+        {
+          skillName: "HTML5",
+          fontAwesomeClassname: "simple-icons:html5",
+          style: {
+            color: "#E34F26",
+          },
+        },
+        {
+          skillName: "CSS3",
+          fontAwesomeClassname: "fa-css3",
+          style: {
+            color: "#1572B6",
+          },
+        },
+        {
+          skillName: "JavaScript",
+          fontAwesomeClassname: "simple-icons:javascript",
+          style: {
+            backgroundColor: "#FFFFFF",
+            color: "#F7DF1E",
+          },
+        },
+        
+        {
+          skillName: "ReactJS",
+          fontAwesomeClassname: "simple-icons:react",
+          style: {
+            color: "#61DAFB",
+          },
+        },
+        
+       
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#439743",
+          },
+        },
+        
+        {
+          skillName: "Android App Development",
+          fontAwesomeClassname: "simple-icons:android",
+          style: {
+            color: "#3DDC84",
+          },
+        },
+       
+        
+
+        {
+          skillName: "Wordpress",
+          fontAwesomeClassname: "simple-icons:wordpress",
+          style: {
+            color: "#207297",
+          },
+        },
+
+        {
+          skillName: "Git",
+          fontAwesomeClassname: "simple-icons:github",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "App Script",
+          fontAwesomeClassname: "simple-icons:google",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "Python",
+          fontAwesomeClassname: "simple-icons:python",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "Springboot",
+          fontAwesomeClassname: "simple-icons:springboot",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        
+      ],
+    },
+
+    {
+      title: "Hardware Skills",
+      fileName: "CloudInfraImg",
+      skills: [
+        "🔹 I have a deep passion for electronics, VLSI and Integrated Circuits, Semiconductor Manufacturing, Physical Design, PCB Design, MATLAB, AutoDesk Fusion 360, Xilinx, Verilog, Proteus, NI Multisim exploring their intricate workings and applications. ",
+        "🔹 Fascinated by network protocols such as OSI, TCP/IP, TCP, UDP and the complexities they present in modern connectivity. I thrive on understanding and unraveling the intricacies of network systems.",
+        
+      ],
+      softwareSkills: [
+        {
+          skillName: "NI Multisim",
+          fontAwesomeClassname: "simple-icons:multisim",
+          style: {
+            color: "#4285F4",
+          },
+        },
+        {
+          skillName: "Autodesk Fusion 360",
+          fontAwesomeClassname: "simple-icons:autodesk",
+          style: {
+            color: "#FF9900",
+          },
+        },
+        {
+          skillName: "Proteus",
+          fontAwesomeClassname: "simple-icons:proteus",
+          style: {
+            color: "#38AFBB",
+          },
+        },
+        {
+          skillName: "Xilinx",
+          fontAwesomeClassname: "simple-icons:xilinx",
+          style: {
+            color: "#6863A6",
+          },
+        },
+        
+        
+      ],
+    },
+    {
+      title: "Data Science & Data Visualization",
+      fileName: "DataScienceImg",
+      skills: [
+        "🔹 Experience of working with Plotly & Google Sheets Data Visualization tools",
+        
+      ],
+      softwareSkills: [
+        {
+          skillName: "Python",
+          fontAwesomeClassname: "ion-logo-python",
+          style: {
+            backgroundColor: "transparent",
+            color: "#3776AB",
+          },
+        },
+        {
+          skillName: "Plotly",
+          fontAwesomeClassname: "simple-icons:plotly",
+          style: {
+            backgroundColor: "white",
+            color: "#D00000",
+          },
+        },
+        
+      ],
+    },
+  ],
+};
+
+const degrees = {
+  degrees: [
+    {
+      title: "Sri Krishna College of Engineering and Technology",
+      subtitle: "BE Electronics and Communication Engineering",
+      logo_path: "skcetlogo.png",
+      alt_name: "DAL",
+      duration: "  2021 - 2025",
+      descriptions: [
+        "🔹 Academic Excellent, Topper & Rank Holder I",
+        "🔹 SGPA: 9.73/10",
+        "🔹 CGPA: 9.2/10",
+        "🔹 Acquired hands on skills in Electronics, Networking and digital Communication",
+        "🔹 Worked on projects on courses like Web Development.",
+      ],
+      website_link: "https://www.skcet.ac.in/",
+    },
+    {
+      title: "Aishwarya Institutions & Dakshina Bharat Hindi Prachar Sabha",
+      subtitle: "BA Hindi Language and Literature",
+      logo_path: "dbhps.png",
+      alt_name: "SETI",
+      duration: "2017 - 2021",
+      descriptions: [
+        "🔹 Kavi Parichaya, ERC, Nibandh Lekhan, Kahani",
+        "🔹 Vyaakaran, Charitra Chitran, Vivechnathmak Lekhan, Patra Lekhan",
+        "🔹 Percentage: 91%",
+      ],
+      website_link: "https://www.skcet.ac.in/",
+    },
+  ],
+};
+
+const certifications = {
+  certifications: [
+    
+    
+    {
+      title: "Google Digital Marketing",
+      subtitle: "2020",
+      logo_path: "google_logo.png",
+      certificate_link: " ",
+      alt_name: "Google",
+      color_code: "#1e70c1",
+    },
+    {
+      title: "Computer on Office Automation",
+      subtitle: "2020",
+      logo_path: "dote.png",
+      certificate_link: " ",
+      alt_name: "Google",
+      color_code: "#ffbfae",
+    },
+    {
+      title: "TNGTE Technical Certification",
+      subtitle: "2019 - 2021",
+      logo_path: "dote.png",
+      certificate_link: " ",
+      alt_name: "Google",
+      color_code: "#b190b0",
+    },
+    {
+      title: "Celonis Foundations",
+      subtitle: "eptember 2023",
+      logo_path: "celonis.png",
+      certificate_link:
+        "https://www.coursera.org/account/accomplishments/records/25JXRB2RWHRX",
+      alt_name: "Google",
+      color_code:  "#fffbf3",
+    },
+    
+    {
+      title: "Getting Started with MongoDB Atlas",
+      subtitle: "Septedmber 2023",
+      logo_path: "mongo.png",
+      certificate_link: "https://graduation.udacity.com/confirm/HLE7K5V3",
+      alt_name: "Google",
+      color_code: "#C5E2EE",
+    },
+    {
+      title: "MongoDB and the Document Model",
+      subtitle: "September 2023",
+      logo_path: "mongo.png",
+      certificate_link: " ",
+      alt_name: "InOut",
+      color_code: "#fffbf3",
+    },
+  ],
+};
+
+// Experience Page
+const experience = {
+  title: "Experience",
+  subtitle: "Work, Internship and Volunteership",
+  description:
+    "I've completed two internships. I've mostly done projects on my own and I am actively looking for internships. I love organizing workshops to share my knowledge with others.",
+  header_image_path: "experience.svg",
+  sections: [
+    {
+      title: "Work",
+      experiences: [
+        {
+          title: "Project Intern",
+          company: "ISRO Propulsion Complex (IPRC)",
+          company_url: "https://www.iprc.gov.in/",
+          logo_path: "iprc.png",
+          duration: "January 2023",
+          location: "Ahmedabad",
+          description:
+            "Done a project on Image Capturing Application for Engine Test using Web Enabled Controller with Structured Text Language.",
+          color: "#2962FF",
+        },
+        {
+          title: "COE Officer",
+          company: "STI Technical Institute",
+          company_url: "", //to be updated with sti website
+          logo_path: "dote.png",
+          duration: "February 2020 - Ongoing",
+          location: "Tamil Nadu, India",
+          description:
+            "Controller of Examinations (COE), Principal officer-in-charge of the conduct of examinations of the Technical Institute, Central Valuation and declaration of their results. Type Instructor & Master. Computer on Office Automation Trainer.",
+          color: "#f10000",
+        },
+        {
+          title: "Cisco CA",
+          company: "Cisco",
+          company_url: "https://www.cisco.com/",
+          logo_path: "cisco2.png",
+          duration: "August 2023 - Ongoing",
+          location: "Tamil Nadu, India",
+          description:
+            "University Representative & Campus Ambassador for the conduct of Technical Events at Sri Krishna College of Engineering and Technology",
+          color: "#f10000",
+        },
+        
+      ],
+    },
+    {
+      title: "Internships",
+      experiences: [
+        {
+          title: "Intern",
+          company: "ISRO Propulsion Complex (IPRC)",
+          company_url: "https://www.iprc.gov.in/",
+          logo_path: "iprc.png",
+          duration: "May 2019 - Aug 2019",
+        
+          description:
+            "Sensors and Calibration of transducers. Systems and equipments for Assembly Integration for Cryogenic stages of GSLV Mk II and GSLV Mk III. Instrumentation systems and Command & Control systems of Main Engine and Stage Test Facility for testing Cryogenic Engine.",
+          color: "#ee3c26",
+        },
+        {
+          title: "Embedded Systems Intern",
+          company: "Emertxe",
+          company_url: "https://www.emertxe.com/",
+          logo_path: "emertxe.png",
+          duration: "February 2024 - Ongoing",
+          
+          description:
+            "Embedded Systems and its Applications with real-time projects",
+          color: "#0071C5",
+        },
+      ],
+    },
+    {
+      title: "Volunteerships",
+      experiences: [
+        {
+          title: "Chairman",
+          company: "IEEE Student Branch SKCET - 61261",
+          company_url: "https://www.ieee.org/",
+          logo_path: "ieee1.png",
+          duration: "Summer of 2017 / 18 / 20",
+          location: " ",
+          description:
+            "Overall Coordinator of the IEEE Madras Section Hub Congress. Mentorship responsibilities were to help the Student Branch plan the Project Funding, Annual Activities, Student Branch Reporting, ensure smooth progress of the Student Branch.",
+          color: "#4285F4",
+        },
+        {
+          title: "Coordinator of District Teams",
+          company: "Youth United Council of India",
+          company_url: "https://youthunitedcouncilofindia.org/",
+          logo_path: "yuci.png",
+          duration: "January 2024 - Ongoing",
+          
+          description:
+            "Organize the teams of various clubs all over the state of Tamil Nadu.",
+          color: "#D83B01",
+        },
+        {
+          title: "Head of Human Resource (HR)",
+          company: "Youth United Council of India",
+          company_url: "https://youthunitedcouncilofindia.org/",
+          logo_path: "yuci.png",
+          duration: "January 2024 - Ongoing",
+          
+          description:
+            "Talent Acquisition Team. Selecting the Candidates best suited for the available positions at YUCI",
+          color: "#D83B01",
+        },
+      ],
+    },
+  ],
+};
+
+// Projects Page
+const projectsHeader = {
+  title: "Projects",
+  description:
+    "My projects makes use of vast variety of latest technology tools. My best experience is to create Data Science projects and Web Development Projects. Below are some of my projects. Note that not all of the mentioned projects are on GitHub yet.",
+  avatar_image_path: "projects_image.svg",
+};
+
+// Contact Page
+const contactPageData = {
+  contactSection: {
+    title: "Hire me?",
+    profile_image_path: "akasha.jpg",
+    description:
+      "I'm always open to opportunities to contribute and learn together. Let's connect and explore how we can make a difference.",
+  },
+  blogSection: {
+    title: "Love my work?",
+    subtitle:
+      "Let's work together and build projects. I'm all ears to your innovations!",
+    link: "https://wa.me/918220093377",
+    avatar_image_path: "blogs_image.svg",
+  },
+};
+
+const projects = {
+  data: [
+    {
+      name: "Knowly",
+      url: "https://fabulous-dragon-006c8c.netlify.app/",
+      description: "A customized application from AtoZ for a Government Approved Technical Institute and an educational website offering wide range of courses and Result Management.",
+      languages: [
+        {
+          name: "React.js",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "TypeScript",
+          iconifyClass: "devicon-typescript",
+        },
+        {
+          name: "Google App Script",
+          iconifyClass: "devicon-google",
+        },
+        {
+          name: "MySQL",
+          iconifyClass: "logos-mysql",
+        },
+        {
+          name: "Springboot",
+          iconifyClass: "logos-spring",
+        },
+      ],
+    },
+
+
+    {
+      name: "Result Management, Data Management & Visualization",
+      url: "https://oia.link/FinalResults",
+      description:
+        "Summary of Test Reports, Results Management & publishing and data statistics visualization",
+      languages: [
+        {
+          name: "Google App Script",
+          iconifyClass: "devicon-google",
+        },
+        {
+          name: "ReactJS",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        {
+          name: "Git",
+          iconifyClass: "devicon-git",
+        },
+        {
+          name: "MySQL",
+          iconifyClass: "logos-mysql",
+        },
+        
+      ],
+    },
+
+    {
+      name: "Development of NTRIP, GNSS RTK Correction Network",
+      url: "https://github.com/akashnb0",
+      description:
+        "NTRIP Server, Caster and Client Application for Accurate GPS Positioning in Centimeter level.",
+      languages: [
+        {
+          name: "ReactJS",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        {
+          name: "JSON Web Tokens",
+          iconifyClass: "logos-jwt",
+        },
+        {
+          name: "Nodejs",
+          iconifyClass: "logos-nodejs",
+        },
+        {
+          name: "Git",
+          iconifyClass: "devicon-git",
+        },
+        {
+          name: "MongoDB",
+          iconifyClass: "devicon-mongodb",
+        },
+        {
+          name: "Python",
+          iconifyClass: "devicon-python",
+        },
+        
+        
+      ],
+    },
+
+    {
+      name: "Dia'b",
+      url: "https://github.com/akashnb0",
+      description:
+        "A personalised Food Delivery Application for Diabetes with pill remainders facilities and post appointment care with timely meal suggest.",
+      languages: [
+        {
+          name: "Android",
+          iconifyClass: "logos-android",
+        },
+        {
+          name: "Postman",
+          iconifyClass: "logos-postman",
+        },
+        
+      ],
+    },
+    {
+      name: "EV Fuel Station Tracker",
+      url: "https://github.com/akashnb0",
+      description: "A mobile application for EV users and admins for navigation and charging",
+      languages: [
+        {
+          name: "Android",
+          iconifyClass: "logos-android",
+        },
+        {
+          name: "Postman",
+          iconifyClass: "logos-postman",
+        },
+        {
+          name: "Figma",
+          iconifyClass: "logos-figma",
+        },
+      ],
+    },
+    {
+      name: "AI Based Smart Patient Queue Scheduling in Hospitals",
+      url: "https://github.com/akashnb0",
+      description: "A web application tailored to efficiently manage patient queues in hospitals",
+      languages: [
+        {
+          name: "ReactJS",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-jwt",
+        },
+        {
+          name: "MySQL",
+          iconifyClass: "logos-mysql",
+        },
+        {
+          name: "AI",
+          iconifyClass: "logos-ai",
+        },
+        
+      ],
+    },
+    {
+      name: "Smart Wristband Technology for Ground Personnel",
+      url: "https://github.com/akashnb0",
+      description:
+        "An IoT-based smart wearable technology for Ground Personnel tracking during bandobast",
+      languages: [
+        {
+          name: "reactjs",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        {
+          name: "Google Maps",
+          iconifyClass: "devicon-google",
+        },
+        {
+          name: "MySQL",
+          iconifyClass: "devicon-mysql",
+        },
+        
+      ],
+    },
+    
+    {
+      name: "Profile Output Generation",
+      url: "https://github.com/akashnb0",
+      description:
+        "Profile Output Generation using Image Capturing Application for Engine Test using Web Enabled Controller with Structured Text Language",
+      languages: [
+        
+        
+      ],
+    },
+    
+    
+    
+  ],
+};
+
+export {
+  settings,
+  greeting,
+  socialMediaLinks,
+  skills,
+  degrees,
+  certifications,
+  experience,
+  projectsHeader,
+  contactPageData,
+  projects,
+};

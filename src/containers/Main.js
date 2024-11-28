f7d7e4e4b318860a0ce4077bcceb953b4f7f2c43
Akash S <108513566+akashnb0@users.noreply.github.com>
2024-11-28 12:22:19 +0530

    Main.js

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -9,6 +9,238 @@ import Projects from "../pages/projects/Projects";
 import { settings } from "../portfolio.js";
 
 export default function Main(propss) {
+  const redirectTo = (url) => {
+    window.location.href = url;
+    return null;
+  };
+
+  if (settings.isSplash) {
+    return (
+      <div>
+        <HashRouter basename="/">
+          <Switch>
+            {/* Existing Routes */}
+            <Route
+              path="/"
+              exact
+              render={(props) => (
+                <Splash
+                  {...props}
+                  theme={propss.theme}
+                  setTheme={propss.setTheme}
+                />
+              )}
+            />
+            <Route
+              path="/home"
+              render={(props) => (
+                <Home
+                  {...props}
+                  theme={propss.theme}
+                  setTheme={propss.setTheme}
+                />
+              )}
+            />
+            <Route
+              path="/experience"
+              exact
+              render={(props) => (
+                <Experience
+                  {...props}
+                  theme={propss.theme}
+                  setTheme={propss.setTheme}
+                />
+              )}
+            />
+            <Route
+              path="/education"
+              render={(props) => (
+                <Education
+                  {...props}
+                  theme={propss.theme}
+                  setTheme={propss.setTheme}
+                />
+              )}
+            />
+            <Route
+              path="/contact"
+              render={(props) => (
+                <Contact
+                  {...props}
+                  theme={propss.theme}
+                  setTheme={propss.setTheme}
+                />
+              )}
+            />
+            <Route
+              path="/projects"
+              render={(props) => (
+                <Projects
+                  {...props}
+                  theme={propss.theme}
+                  setTheme={propss.setTheme}
+                />
+              )}
+            />
+
+            {/* Redirect Routes */}
+            <Route
+              path="/siliconresume"
+              render={() =>
+                redirectTo(
+                  "https://drive.google.com/file/d/1J5-YE1YkSQyRM_YoRx3T53o8iONh8ajN/view?usp=drive_link"
+                )
+              }
+            />
+            <Route
+              path="/networkresume"
+              render={() =>
+                redirectTo(
+                  "https://drive.google.com/file/d/1GW7dKKvHjl6KP4z3mHJ905YTYSlpR3de/view?usp=drive_link"
+                )
+              }
+            />
+            <Route
+              path="/hardwareresume"
+              render={() =>
+                redirectTo(
+                  "https://drive.google.com/file/d/1dfzfdbxTJQ6-7nzkYy0xTPK4Al9_sxw4/view?usp=sharing"
+                )
+              }
+            />
+            <Route
+              path="/links"
+              render={() =>
+                redirectTo(
+                  "https://drive.google.com/file/d/1dfzfdbxTJQ6-7nzkYy0xTPK4Al9_sxw4/view?usp=sharing"
+                )
+              }
+            />
+          </Switch>
+        </HashRouter>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <HashRouter basename="/">
+          <Switch>
+            {/* Existing Routes */}
+            <Route
+              path="/"
+              exact
+              render={(props) => (
+                <Home
+                  {...props}
+                  theme={propss.theme}
+                  setTheme={propss.setTheme}
+                />
+              )}
+            />
+            <Route
+              path="/home"
+              render={(props) => (
+                <Home
+                  {...props}
+                  theme={propss.theme}
+                  setTheme={propss.setTheme}
+                />
+              )}
+            />
+            <Route
+              path="/experience"
+              exact
+              render={(props) => (
+                <Experience
+                  {...props}
+                  theme={propss.theme}
+                  setTheme={propss.setTheme}
+                />
+              )}
+            />
+            <Route
+              path="/education"
+              render={(props) => (
+                <Education
+                  {...props}
+                  theme={propss.theme}
+                  setTheme={propss.setTheme}
+                />
+              )}
+            />
+            <Route
+              path="/contact"
+              render={(props) => (
+                <Contact
+                  {...props}
+                  theme={propss.theme}
+                  setTheme={propss.setTheme}
+                />
+              )}
+            />
+            <Route
+              path="/projects"
+              render={(props) => (
+                <Projects
+                  {...props}
+                  theme={propss.theme}
+                  setTheme={propss.setTheme}
+                />
+              )}
+            />
+
+            {/* Redirect Routes */}
+            <Route
+              path="/siliconresume"
+              render={() =>
+                redirectTo(
+                  "https://drive.google.com/file/d/1J5-YE1YkSQyRM_YoRx3T53o8iONh8ajN/view?usp=drive_link"
+                )
+              }
+            />
+            <Route
+              path="/networkresume"
+              render={() =>
+                redirectTo(
+                  "https://drive.google.com/file/d/1GW7dKKvHjl6KP4z3mHJ905YTYSlpR3de/view?usp=drive_link"
+                )
+              }
+            />
+            <Route
+              path="/hardwareresume"
+              render={() =>
+                redirectTo(
+                  "https://drive.google.com/file/d/1dfzfdbxTJQ6-7nzkYy0xTPK4Al9_sxw4/view?usp=sharing"
+                )
+              }
+            />
+            <Route
+              path="/links"
+              render={() =>
+                redirectTo(
+                  "https://drive.google.com/file/d/1dfzfdbxTJQ6-7nzkYy0xTPK4Al9_sxw4/view?usp=sharing"
+                )
+              }
+            />
+          </Switch>
+        </HashRouter>
+      </div>
+    );
+  }
+}
+
+
+/* import React from "react";
+import { Route, Switch, HashRouter } from "react-router-dom";
+import Home from "../pages/home/HomeComponent";
+import Splash from "../pages/splash/Splash";
+import Education from "../pages/education/EducationComponent";
+import Experience from "../pages/experience/Experience";
+import Contact from "../pages/contact/ContactComponent";
+import Projects from "../pages/projects/Projects";
+import { settings } from "../portfolio.js";
+
+export default function Main(propss) {
   if (settings.isSplash) {
     return (
       <div>
@@ -162,4 +394,4 @@ export default function Main(propss) {
       </div>
     );
   }
-}
+}    */
